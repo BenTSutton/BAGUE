7c296eac328ccfc239021e86c22df7e00434e5f2
EwanBrownTown <ewan.brown1999@gmail.com>
2026-03-17 22:43:52 +0000

    Lots of changes
    Whoops, i did it again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,150 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public Rigidbody2D rb;
+    //Basic Movement
     public float moveSpeed = 5f;
-    public float jumpForce = 10f;
+    public float sprintSpeed = 8f;
+    public float jumpForce = 8f;
 
     public float moveInput;
-
+    //Ground Check deez balls
     public Transform groundCheck;
-    public float groundCheckRadius= 0.2f;
+    public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
     public bool isGrounded;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // STAMINA (In the Bedroom ;)
+    public float maxStamina = 100f;
+    public float stamina;
+    public float sprintStaminaDrain = 20f;
+    public float dashStaminaCost = 30f;
+    public float staminaRegen = 15f;
+    public float staminaRegenDelay = 1.5f;
+    private float regenTimer = 0f;
+
+    // DASH 
+    public float dashForce = 25f;
+    public float dashTime = 0.2f;
+    public float dashCooldown = 2f;
+
+    // U&I Date? (UI)
+    public Slider staminaBar;
+    public Image staminaFill;
+    public TMP_Text staminaText;
+    private float displayedStamina;
+    public float staminaSmoothSpeed = 6f;
+
+    private bool isDashing = false;
+    private bool canDash = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        stamina = maxStamina;
+        displayedStamina = maxStamina;
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveInput = Input.GetAxis("Horizontal");
 
-    }
-
-    void FixedUpdate()
-    {
-        //Checks if the GroundCheck object overlaps with ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        //Moves the player horizontally
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-       
-        //Checks if player tries to jump and is grounded, and jumps if so
+        // Jump 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-   }
+
+        // Dash
+       if (Input.GetKeyDown(KeyCode.LeftControl) && canDash && stamina >= dashStaminaCost)
+    {
+        stamina -= dashStaminaCost;
+
+        regenTimer = staminaRegenDelay;
+
+        StartCoroutine(Dash());
+    }
+
+        // Smooth stamina bar
+        displayedStamina = Mathf.Lerp(displayedStamina, stamina, Time.deltaTime * staminaSmoothSpeed);
+        staminaBar.value = displayedStamina / maxStamina;
+
+        //Colour Changing Stamina Bar & Alfie Sucks ;)
+        float staminaPercent = stamina / maxStamina;
+
+        if (staminaPercent > 0.6f)
+    {
+        staminaFill.color = Color.green;
+    }
+        else if (staminaPercent > 0.3f)
+    {
+        staminaFill.color = Color.yellow;
+    }
+        else
+    {
+        staminaFill.color = Color.red;
+    }
+        staminaText.text = Mathf.RoundToInt(stamina) + " / " + maxStamina;
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isDashing) return;
+
+        float speed = moveSpeed;
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+                speed = sprintSpeed;
+                stamina -= sprintStaminaDrain * Time.fixedDeltaTime;
+
+            regenTimer = staminaRegenDelay;
+        }
+
+        // Regen delay logic
+        if (regenTimer > 0)
+            {
+                regenTimer -= Time.fixedDeltaTime;
+            }
+        else
+            {
+                stamina += staminaRegen * Time.fixedDeltaTime;
+            }
+
+            // CLAMP STAMINA HERE
+            stamina = Mathf.Clamp(stamina, 0, maxStamina);
+
+            rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+        float dashDirection = moveInput;
+
+        if (dashDirection == 0)
+        {
+            dashDirection = 1;
+        }
+
+        rb.linearVelocity = new Vector2(dashDirection * dashForce, rb.linearVelocity.y);
+
+        yield return new WaitForSeconds(dashTime);
+
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+
+        canDash = true;
+    }
 }
