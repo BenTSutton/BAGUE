85a927f8490c63893e721ab345d31f9944dc2874
Alfie Thomasson <alfiethomasson0@gmail.com>
2026-03-25 20:42:53 +0000

    Add DebugPanel in CombatScene to see when Debug is active

---
+++ Assets/Scripts/UI/DebugPanel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DebugPanel : MonoBehaviour
+{
+    void Awake()
+    {
+        if (GameManager.Instance != null)
+        {
+            gameObject.SetActive(GameManager.Instance.winCombatDebug);
+        }
+        else
+        {
+            // Test scene, likely no GameManager
+            gameObject.SetActive(true);
+        }
+    }
+}
